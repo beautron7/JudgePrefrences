43cbe43a51052586edbc6159d735951a6b6042e7
Beau VanDenburgh <beautron7@gmail.com>
2018-04-24 16:42:42 -0500

    Finished downloading, Making training data

--- downloadWikis.js
+++ downloadWikis.js
@@ -23,8 +23,15 @@ readCSV("judges.csv").then(sitemapCSV=>{
   defaultRequestOptions = {
     transform: cheerio.load,//when html is received, run cheerio.load(html), and pass to fcn.
   };
-
-  getJudge(0);//start the loop
+  fs.readFile("./paradigms/progress.txt")
+    .then((data)=>{
+      console.log("Starting at "+data)
+      getNextJudge(data-2);//start the loop
+    })
+    .catch((e)=>{
+      console.log(e)
+      getNextJudge(0)
+    })
 })
 
 function getNextJudge(i){
@@ -70,17 +77,38 @@ function getJudge(i) {
 }
 
 
+let CtrlCFileTimeout = -1;
 
 process.on('SIGINT', function() {//doesn't work.
-  console.log("Saving progress to ./paradigms/progress.txt");
-  getNextJudge =(i)=>{
-    fs.writeFile("./paradigms/progress.txt",i,"utf8").then(()=>{
-      setTimeout(() => {
-        process.exit(1);
+  if(CtrlCFileTimeout == -1){
+    setTimeout(() => {
+      console.log("Saving progress to ./paradigms/progress.txt in ");
+    }, 100);
+    getNextJudge =(i)=>{//fix this later
+      CtrlCFileTimeout = setTimeout(() => {console.log("5");
+        CtrlCFileTimeout = setTimeout(() => {console.log("4");
+          CtrlCFileTimeout = setTimeout(() => {console.log("3");
+            CtrlCFileTimeout = setTimeout(() => {console.log("2");
+              CtrlCFileTimeout = setTimeout(() => {console.log("1");
+                fs.writeFile("./paradigms/progress.txt",i,"utf8").then(()=>{
+                  console.log("saved file to the thing");
+                  process.exit(0);
+                }).catch(()=>{
+                  console.log("failed to save progress");
+                  throw new Error("Exiting");
+                  process.exit(0)
+                })
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
       }, 1000);
-    })
-  };
-  
+    };
+  } else {
+    clearTimeout(CtrlCFileTimeout)
+    console.log("Quitting before saving");
+    process.exit(0)
+  }
 });
 
 
